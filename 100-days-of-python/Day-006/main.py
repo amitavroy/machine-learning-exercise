3928c718be3657888b96ee75ae9c10c85286886d
Amitav Roy <reachme@amitavroy.com>
2024-01-04 07:05:39 +0530

    Day006-Finished the high lo game Using while, completed the high lo game

--- 100-days-of-python/Day-006/main.py
+++ 100-days-of-python/Day-006/main.py
@@ -20,3 +20,14 @@ while True:
     else:
         print('Need to enter h, l or c')
     guesses = guesses + 1
+
+# assignment code
+number = 5
+multiplier = 8
+answer = 0
+ 
+# add your loop after this comment
+for i in range(multiplier):
+    answer += number
+ 
+print(answer)
