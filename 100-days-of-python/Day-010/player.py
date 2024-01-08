4e2524c0bfef14248df060a27977d2f883b8f808
Amitav Roy <reachme@amitavroy.com>
2024-01-08 22:49:52 +0530

    Day-010 Completed Lists and Tuples Finished lists and tuples and now will move to functions

---
+++ 100-days-of-python/Day-010/player.py
@@ -0,0 +1,28 @@
+from main import albums
+
+SONGS_LIST_INDEX = 3
+SONG_TITLE_INDEX = 1
+
+while True:
+    print("Please choose your album (invalid choice exits):")
+    for index, (title, artist, year, songs) in enumerate(albums):
+        print("{}: {}".format(index + 1, title))
+
+    choice = int(input())
+    if 1 <= choice <= len(albums):
+        songs_list = albums[choice -1][SONGS_LIST_INDEX]
+    else:
+        break
+
+    print("Please choose your song:")
+    for index, (track_number, song) in enumerate(songs_list):
+        print("{}: {}".format(index + 1, song))
+
+    song_choice = int(input())
+    if 1 <= song_choice <= len(songs_list):
+        title = songs_list[song_choice - 1][SONG_TITLE_INDEX]
+    else:
+        break
+
+    print("Playing {}".format(title))
+    print("=" * 40)
