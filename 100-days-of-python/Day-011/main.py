95480c6a9d23c62d0a272af45502f0910d962ae9
Amitav Roy <reachme@amitavroy.com>
2024-01-09 21:55:06 +0530

    Day-011 Continue with functions
    Lot of things to cover in functions.
    The Docblock style of Python is very nice and very different as well.

---
+++ 100-days-of-python/Day-011/main.py
@@ -0,0 +1,16 @@
+def helloWorld(name):
+    print("Hello there {}".format(name))
+
+def is_palindrome(string):
+    """
+    Checks if the provided string is a palindrome or not.
+    :param string: The string that will be checked whether it is Palindrome or not.
+    :return: Returns a boolean value whether the string is palindrome or not.
+    """
+    new_string = string.casefold()
+    return new_string[::-1] == new_string
+
+
+helloWorld("Amitav")
+print()
+print(is_palindrome('Dad'))
