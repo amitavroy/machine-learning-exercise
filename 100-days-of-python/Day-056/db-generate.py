662a85f32953a0a7c68b03f3c1043675c8072933
Amitav Roy <reachme@amitavroy.com>
2024-02-26 19:52:12 +0530

    Day-056 Script improvements
    Working on converting the script to different parts.
    The DB is generated in one file.
    While the chat can happen in other file.

---
+++ 100-days-of-python/Day-056/db-generate.py
@@ -0,0 +1,23 @@
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+
+DB_FAISS_PATH="data/db_faiss"
+
+# Download Sentence Transformers Embedding From Hugging Face
+# Loading it here so that I can use it in both cases i.e.
+# when the vector store is present and not present
+embeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
+
+# Generating the FAISS DB here so that the main code is simpler.
+loader = CSVLoader(file_path="data/faq.csv", encoding="utf-8", csv_args={'delimiter': ','})
+data = loader.load()
+
+# Split the text into Chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+text_chunks = text_splitter.split_documents(data)
+
+# Converting the text Chunks into embeddings and saving the embeddings into FAISS Knowledge Base
+docsearch = FAISS.from_documents(text_chunks, embeddings)
+docsearch.save_local(DB_FAISS_PATH)
